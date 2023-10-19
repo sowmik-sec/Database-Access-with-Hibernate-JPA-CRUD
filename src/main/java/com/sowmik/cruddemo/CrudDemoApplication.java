2164150e48a2c50ddff29023b9558c8134c5792c
sowmik-sec <sowmik.sec@gmail.com>
2023-10-19 10:17:17 +0600

    saving a java object with JPA

--- src/main/java/com/sowmik/cruddemo/CrudDemoApplication.java
+++ src/main/java/com/sowmik/cruddemo/CrudDemoApplication.java
@@ -1,5 +1,7 @@
 package com.sowmik.cruddemo;
 
+import com.sowmik.cruddemo.dao.StudentDAO;
+import com.sowmik.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,22 @@ public class CrudDemoApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
         return runner -> {
-            System.out.println("Hello World");
+            createStudent(studentDAO);
         };
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+        // create the student object
+        System.out.println("Creating new student object...");
+        Student tempStudent = new Student("Paul", "Doe", "paul@doe.com");
+
+        // save the student object
+        System.out.println("Saving the student ...");
+        studentDAO.save(tempStudent);
+
+        // display id of the saved student
+        System.out.println("Saved student. Generated id: "+tempStudent.getId());
     }
 }
---
+++ src/main/java/com/sowmik/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.sowmik.cruddemo.dao;
+
+import com.sowmik.cruddemo.entity.Student;
+
+public interface StudentDAO {
+    void save(Student theStudent);
+}
