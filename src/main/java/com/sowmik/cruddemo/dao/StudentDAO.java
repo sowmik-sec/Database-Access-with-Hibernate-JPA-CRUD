de826ff46d299c1811c41a700ea072de5a21e326
sowmik-sec <sowmik.sec@gmail.com>
2023-10-19 20:42:06 +0600

    querying objects with JPA

--- src/main/java/com/sowmik/cruddemo/dao/StudentDAO.java
+++ src/main/java/com/sowmik/cruddemo/dao/StudentDAO.java
@@ -2,7 +2,11 @@ package com.sowmik.cruddemo.dao;
 
 import com.sowmik.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
     void save(Student theStudent);
     Student findById(Integer id);
+    List<Student> findAll();
+    List<Student> findByLastName(String theLastName);
 }
